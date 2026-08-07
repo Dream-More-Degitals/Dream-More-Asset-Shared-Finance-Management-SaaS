15706b711d27974548445e8a6217fc834d19f322
Kal-kian <kalkidanmisgadu@gmail.com>
2026-08-07 12:40:07 +0300

    Create sidebar

---
+++ components/layout/sidebar.tsx
@@ -0,0 +1,11 @@
+import { Home, Users, Settings } from "lucide-react";
+
+export default function Sidebar() {
+  return (
+    <aside>
+      <Home />
+      <Users />
+      <Settings />
+    </aside>
+  );
+}
